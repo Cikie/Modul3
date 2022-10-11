e5730d4b9e55e7e50163af94411f47b6690335dd
Cikie <daoanh123123@gmail.com>
2022-10-11 10:39:28 +0700

    done

---
+++ 4.TemplateAndStaticFile/1.StaticFile/1.StaticFile.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const {parse} = require("qs");
+// you can pass the parameter in the command line. e.g. node static_server.js 3000
+const port = process.argv[2] || 9000;
+
+
+// maps file extention to MIME types
+const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.zip': 'application/zip',
+    '.doc': 'application/msword',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.ttf': 'application/x-font-ttf',}
+
+http.createServer(function (req, res){
+    console.log(`${req.method} ${req.url}`);
+    const parseUrl = url.parse(req.url);
+    const sanitizePath = path.normalize(parseUrl.pathname).replace(/^(\.\.[\/\\])+/, '')
+
+    let pathname = path.join(__dirname,sanitizePath);
+
+    fs.exists(pathname,function (exist){
+        if (!exist  ){
+            res.statusCode = 404;
+            res.end(`File ${pathname} not found!!`)
+            return
+        }
+        if (fs.statSync(pathname).isDirectory()){
+            pathname += '/index.html'
+        }
+        fs.readFile(pathname,function (err,data){
+            if (err){
+                res.statusCode = 500;
+                res.end(`Error getting file ${err}.`)
+            }else {
+                const ext = path.parse(pathname).ext;
+                res.setHeader(mimeType[ext]||'text/plain');
+                res.end(data)
+            }
+        })
+    })
+}).listen(parseInt(port));
+console.log(`Sever listening on port ${port}`)
---
+++ 4.TemplateAndStaticFile/3.NodeJS/sever.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+let sever = http.createServer(((req, res) => {
+    fs.readFile('./NodeJS/index.html', 'utf-8', function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'})
+            return res.end('404 Not Found')
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}));
+sever.listen('8080', function () {
+    console.log(`Sever is running on local Host 8080 !!`)
+});
