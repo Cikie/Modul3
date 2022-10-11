40534f96427fdc7c845883d96610620735e496b4
Cikie <daoanh123123@gmail.com>
2022-10-11 11:01:00 +0700

    done

---
+++ 4.TemplateAndStaticFile/TemplateHTML/sever.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+let sever = http.createServer(((req, res)=>{
+    fs.readFile ('./index.html',"utf-8" , function (err,data){
+        if (err){
+            res.writeHead(404,{'Content-Type': 'text/html'});
+            return res.end('404 Not Found');
+        }
+        let username = 'Admin';
+        data = data.replace('{username}',username)
+
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end()
+    })
+}));
+sever.listen(3000,function (){
+    console.log(`Sever is Running !!`)
+    }
+)
