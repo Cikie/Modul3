9f967bece54d6b1da7a14f76fe7516210fed999f
Cikie <daoanh123123@gmail.com>
2022-10-12 10:46:08 +0700

    done

---
+++ RoutingWed/UserInfo/info.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+sever = http.createServer(function (req, res) {
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', function (data) {
+        buffer += decoder.write(data)
+    });
+    req.on('end', function (end) {
+        buffer += decoder.end();
+        res.end('Hello NodeJS !!')
+        console.log(buffer)
+    });
+});
+sever.listen(3000, function () {
+    console.log('Sever is Running !!')
+})
